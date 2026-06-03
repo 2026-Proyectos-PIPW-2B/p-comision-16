1694fdf638e075cdc4a8de3a87d0d19d37857606
Andrés Ressia <andresressia0@gmail.com>
2026-06-03 18:37:02 -0300

    Mejora de login.js

--- Main Html/js/login.js
+++ Main Html/js/login.js
@@ -1,42 +1,35 @@
+let usuarios = []
 window.addEventListener("load", iniciar)
 
 function iniciar(){
     usuarios = JSON.parse(localStorage.getItem("Users"))
-    inputs = document.querySelectorAll(".form-control")
+    const inputNombre = document.getElementById("usuario")
+    const inputContrasena = document.getElementById("contrasena")
     const btn = document.getElementById("btn-login")
     btn.addEventListener("click", function(e){
         e.preventDefault()
-        validar(inputs)
+        validar(inputNombre.value, inputContrasena.value)
     })
 
 }
 
-function validar(inputs){
-    for(i in inputs){
-        const input = inputs[i]
-        validarUsuario(input, input.name)
-    }
-}
-
-function validarUsuario(input, name){
-    switch(name){
-        case("usuario"):
-            validarNombre(input.value)
-            break
-    }
+function validar(nombre,contrasena){
+    validarContrasena(validarNombre(nombre), contrasena)
 }
 
 function validarNombre(nombre){
     for(i=0;i<usuarios.length;i++){
-        if(nombre === "a"){
-            console.log("4")
-            validarContrasena()
+        if(nombre === usuarios[i].name){
+            console.log("usuario encontrado")
+            return usuarios[i].password
         }
     }
 }
 
-function validarContrasena(){
-    
+function validarContrasena(contrasenaDeUsuario, contrasena){
+    if(contrasenaDeUsuario === contrasena){
+        console.log("contraseña correcta")
+    }
 }
 
 function crearUsuario(nombre,contrasena){
