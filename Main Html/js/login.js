8798d44152421a5a4d78fa94b49d19c4dfc15f30
Andrés Ressia <andresressia0@gmail.com>
2026-06-04 19:18:48 -0300

    Add files via upload

--- Main Html/js/login.js
+++ Main Html/js/login.js
@@ -8,27 +8,38 @@ function iniciar(){
     const btn = document.getElementById("btn-login")
     btn.addEventListener("click", function(e){
         e.preventDefault()
-        validar(inputNombre.value, inputContrasena.value)
+        validarUsuario(inputNombre, inputContrasena)
     })
 
 }
 
-function validar(nombre,contrasena){
+function validarUsuario(nombre,contrasena){
     validarContrasena(validarNombre(nombre), contrasena)
 }
 
 function validarNombre(nombre){
     for(i=0;i<usuarios.length;i++){
-        if(nombre === usuarios[i].name){
-            console.log("usuario encontrado")
+        if(nombre.value === usuarios[i].name){
+            nombre.classList.remove("is-invalid")
+            nombre.classList.add("is-valid")
             return usuarios[i].password
+        } else{
+            nombre.classList.remove("is-valid")
+            nombre.classList.add("is-invalid")
         }
     }
 }
 
 function validarContrasena(contrasenaDeUsuario, contrasena){
-    if(contrasenaDeUsuario === contrasena){
-        console.log("contraseña correcta")
+    if(contrasenaDeUsuario === contrasena.value){
+        contrasena.classList.remove("is-invalid")
+        contrasena.classList.add("is-valid")
+        setTimeout(() => {
+            location.href = "Catalogo.html"
+        }, 1000);
+    } else{
+        contrasena.classList.remove("is-valid")
+        contrasena.classList.add("is-invalid")
     }
 }
 
