28188b476128ab949bbf38fdb4801bf89d8b1e98
Andrés Ressia <andresressia0@gmail.com>
2026-06-09 18:33:57 -0300

    Add files via upload

---
+++ redireccion.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", redirigir)
+function redirigir(){
+    const usuarioActivo = JSON.parse(localStorage.getItem("usuarioActivo"))
+    if(usuarioActivo === null){
+        window.alert("Debe iniciar sesión")
+        location.href = "login.html"
+    }
+}
+
+setInterval(() => {
+    if(localStorage.getItem("time") === null){
+        localStorage.setItem("time", 0)
+    } else{
+        localStorage.setItem("time", Number(localStorage.getItem("time"))+1)
+    }
+
+    if(localStorage.getItem("time") == localStorage.getItem("timer")){
+        localStorage.setItem("time", 0)
+        localStorage.removeItem("usuarioActivo")
+        redirigir()
+    }
+}, 1000);
