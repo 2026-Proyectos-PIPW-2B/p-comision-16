5e37380c8c603f13841f94e0eb69cb7bed02e6fa
Andrés Ressia <andresressia0@gmail.com>
2026-06-18 18:46:24 -0300

    Add files via upload

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,36 @@
-const listaCarrito = document.getElementById("lista-carrito")
+import { setLocalStorage } from "./moduloLocalStorage.js"
+import { getLocalStorage } from "./moduloLocalStorage.js"
+
+window.addEventListener("load", iniciarCarrito)
+
+function iniciarCarrito(){
+    const listaCarrito = document.getElementById("lista-carrito")
+
+    const productos = getLocalStorage("products")
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const li = document.createElement("li")
+li.classList.add("list-group-item", "text-light", "d-flex", "align-items-center", "border-secondary", "lista-dark-purple")
+
+const div = document.createElement("div")
+div.classList.add("d-flex", "align-items-center")
 
 const img = new Image(50,50)
 img.src = "img/Pala.jpg"
@@ -9,16 +41,55 @@ const producto ={
     nombre: "Pala",
     imagen: img,
     cantidad: 1,
-    precio: "200$"
+    precio: 20
 }
+
+const h6 = document.createElement("h6")
+h6.classList.add("mb-0")
+h6.textContent = producto.nombre
+
+div.appendChild(img)
+div.appendChild(h6)
+
+const divBoton = document.createElement("div")
+divBoton.classList.add("d-flex", "align-items-center", "gap-2", "ms-auto")
+
+const spanCantidad = document.createElement("span")
+spanCantidad.classList.add("badge", "rounded-pill", "bg-secondary", "text-light", "px-3", "py-2")
+spanCantidad.textContent = producto.cantidad
+
+const btnMenos = document.createElement("button")
+const menos = document.createElement("i")
+btnMenos.classList.add("btn", "btn-sm", "btn-outline-light")
+menos.classList.add("bi", "bi-dash-lg")
+btnMenos.append(menos)
+
+
+const spanPrecio = document.createElement("span")
+spanPrecio.classList.add("badge", "bg-secondary", "rounded-pill", "ms-4")
+spanPrecio.textContent = `$${producto.precio}`
 
 const btnMas = document.createElement("button")
 const mas = document.createElement("i")
 btnMas.classList.add("btn", "btn-sm", "btn-outline-light")
 mas.classList.add("bi", "bi-plus-lg")
 btnMas.append(mas)
-const btnMenos = document.createElement("button")
-const menos = document.createElement("i")
-btnMenos.classList.add("btn", "btn-sm", "btn-outline-light")
-menos.classList.add("bi", "bi-dash-lg")
-btnMenos.append(menos)
+
+
+divBoton.appendChild(btnMenos)
+divBoton.appendChild(spanCantidad)
+divBoton.appendChild(btnMas)
+divBoton.appendChild(spanPrecio)
+
+li.appendChild(div)
+li.appendChild(divBoton)
+
+function restar(cantidad){
+    cantidad.textContent = Number(cantidad.textContent)-1
+}
+
+function sumar(cantidad){
+    cantidad.textContent = Number(cantidad.textContent)+1
+}
+
+listaCarrito.appendChild(li)
--- js/moduloLocalStorage.js
+++ js/moduloLocalStorage.js
@@ -5,6 +5,26 @@ const Admin = {
 }
 let usuarios = [Admin]
 
+let productos = []
+const nombreProductos = ["Peluche Kasane Teto", "BlackBerry Bold Touch 9900", "Impresora Portátil Epson WorkForce WF-100", "Globos con Helio", "Destornillador Plano 8x200 Mm Stanley", "Pala Punta Con Mango Anilla", 'Cubiertos "Comunes"', "Platos Blancos", "Fiat 600"]
+const precios = [20,200,1400,10,8,40,6,15,40000]
+const imagenes = ["img/7155-AFmP9L._SY500_.jpg", "img/blackberry-bold-touch-9900-4541-g-alt.jpg", "img/WF-100_C11CE05302_3.jpg", "img/Globos-Helio.png", "img/Destornillador.png", "img/Pala.jpg", "img/Cubiertos.jpg", "img/Platos.jpg", "img/Fiat600.jpg"]
+
+for(const i in nombreProductos){
+    const img = new Image(50,50)
+    img.src = imagenes[i]
+    img.alt = nombreProductos[i]
+    img.height = 50
+    img.classList.add("me-3")
+    const producto ={
+        nombre: nombreProductos[i],
+        imagen: img,
+        precio: precios[i]
+    }
+    productos.push(producto)
+}
+
+
 export function setLocalStorage(item, elementos){
     localStorage.setItem(item,JSON.stringify(elementos))
 }
@@ -20,3 +40,6 @@ export function startLocalStorage(){
         return getLocalStorage("users")
     }
 }
+
+
+setLocalStorage("productos", productos)
