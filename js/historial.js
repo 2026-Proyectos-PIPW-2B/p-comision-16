98a8c2b8bf209557570988af8884b471b0a04dd0
Andres Ressia <andresressia@gmail.com>
2026-06-22 19:51:35 -0300

    modularización de verProductos, para utilizarlo en administración

--- js/historial.js
+++ js/historial.js
@@ -1,4 +1,5 @@
 import { getLocalStorage } from "./moduloLocalStorage.js"
+import { verProductos } from "./moduloVerProductos.js"
 
 window.addEventListener("load", iniciarHistorial)
 
@@ -36,25 +37,4 @@ function crearTabla(historial){
         tableBody.appendChild(tr)
     }
     return tableBody
-}
-
-function verProductos(compra){
-    const modalTabla = document.getElementById("listaProductos")
-    modalTabla.textContent = ""
-    for(const i in compra.productos){
-        const producto = compra.productos[i]
-        const tr = document.createElement("tr")
-
-        const tdProducto = document.createElement("td")
-        tdProducto.textContent = producto.nombre
-        const tdCantidad = document.createElement("td")
-        tdCantidad.textContent = producto.cantidad
-        const tdPrecio = document.createElement("td")
-        tdPrecio.textContent = `$${producto.cantidad*producto.precio}`
-
-        tr.appendChild(tdProducto)
-        tr.appendChild(tdCantidad)
-        tr.appendChild(tdPrecio)
-        modalTabla.appendChild(tr)
-    }
 }
